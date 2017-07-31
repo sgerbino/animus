0f525f4a13979bc0ca14381b5d394273443dad29
Steve Gerbino <steve@gerbino.co>
2017-07-30 23:26:07 -0400

    Removed root path from API creation as it is not needed, renamed some Api members

--- src/cpp/api.cpp
+++ src/cpp/api.cpp
@@ -7,23 +7,31 @@ using namespace animus;
 using namespace std;
 
 shared_ptr<animus_generated::api> animus_generated::api::create_api(
-                                                                    const string &root_path,
-                                                                    const shared_ptr<animus_generated::event_loop> &ui_thread,
-                                                                    const shared_ptr<animus_generated::http> &http_impl,
-                                                                    const shared_ptr<animus_generated::thread_launcher> &launcher) {
+    const shared_ptr<animus_generated::event_loop> &ui_thread,
+    const shared_ptr<animus_generated::http> &http_impl,
+    const shared_ptr<animus_generated::thread_launcher> &launcher)
+{
     const auto ui_runner = make_shared<animus::event_loop>(ui_thread);
     const auto bg_runner = make_shared<animus::animus_loop>(launcher);
-    return make_shared<animus::api>(root_path, ui_runner, bg_runner, http_impl);
+    return make_shared<animus::api>(ui_runner, bg_runner, http_impl);
 }
 
 api::api(
-         const string& root_path,
-         const shared_ptr<task_runner>& ui_runner,
-         const shared_ptr<task_runner>& bg_runner,
-         const shared_ptr<animus_generated::http>& http_client
-         ) :
-ui_thread {ui_runner},
-bg_thread {bg_runner},
-bg_http {http_client, bg_thread}
+    const shared_ptr<task_runner>& ui_runner,
+    const shared_ptr<task_runner>& bg_runner,
+    const shared_ptr<animus_generated::http>& http_impl) :
+        user_interface_dispatcher {ui_runner},
+        daemon_dispatcher {bg_runner},
+        http_client {http_impl, daemon_dispatcher}
 {
+    http_client.get("https://jsonplaceholder.typicode.com/posts", [ui = user_interface_dispatcher] (http_response resp) {
+        if (resp.error) {
+            return;
+        }
+        
+        std::cout << resp.http_code << std::endl;
+        std::cout << resp.data << std::endl;
+        std::cout << std::this_thread::get_id() << " bg thread" << std::endl;
+        ui->post([](){ std::cout << std::this_thread::get_id() << " ui thread" << std::endl; });
+    });
 }
